fe926b586cf51633417f214f8969273481a34007
neo <a18814888787@gmail.com>
2024-09-06 23:49:34 +0800

    feat: add react native safe area context

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import {  Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red-500">
-      <Text>Open up App.tsx test123123 to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaProvider >
+          <View className="flex-1 items-center justify-center bg-red-500">
+              <Text>Open up App.tsx test123123 to start working on your app!</Text>
+              <StatusBar style="auto"/>
+              <Ionicons size={80} name={"bag-add-outline"}/>
+          </View>
+      </SafeAreaProvider>
+
   );
 }
