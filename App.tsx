b9c25fbbd589e0ee837f6b73b843555570c7033f
neo <a18814888787@gmail.com>
2024-09-06 23:57:18 +0800

    feat: add eslint and prettier

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import {  Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
-  return (
-      <SafeAreaProvider >
-          <View className="flex-1 items-center justify-center bg-red-500">
-              <Text>Open up App.tsx test123123 to start working on your app!</Text>
-              <StatusBar style="auto"/>
-              <Ionicons size={80} name={"bag-add-outline"}/>
-          </View>
-      </SafeAreaProvider>
-
-  );
+    return (
+        <SafeAreaProvider>
+            <View className="flex-1 items-center justify-center bg-red-500">
+                <Text>Open up App.tsx test123123 to start working on your app!</Text>
+                <StatusBar style="auto" />
+                <Ionicons size={80} name={'bag-add-outline'} />
+            </View>
+        </SafeAreaProvider>
+    );
 }
