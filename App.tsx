777365558a5d7465e63b1e7d80f2b6bdd67f3869
neo <a18814888787@gmail.com>
2024-09-07 00:09:09 +0800

    feat: add toast and add loadedAssets

--- App.tsx
+++ App.tsx
@@ -2,15 +2,25 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ToastProvider } from 'react-native-toast-notifications';
+import { useLoadedAssets } from './src/hooks/use-loaded-assets';
 
 export default function App() {
+    const isAssetsLoadingComplete = useLoadedAssets();
+
+    if (!isAssetsLoadingComplete) {
+        return null;
+    }
+
     return (
         <SafeAreaProvider>
-            <View className="flex-1 items-center justify-center bg-red-500">
-                <Text>Open up App.tsx test123123 to start working on your app!</Text>
-                <StatusBar style="auto" />
-                <Ionicons size={80} name={'bag-add-outline'} />
-            </View>
+            <ToastProvider duration={2000} animationDuration={250} offsetBottom={100}>
+                <View className="flex-1 items-center justify-center bg-red-500">
+                    <Text>Open up App.tsx test123123 to start working on your app!</Text>
+                    <StatusBar style="auto" />
+                    <Ionicons size={80} name={'bag-add-outline'} />
+                </View>
+            </ToastProvider>
         </SafeAreaProvider>
     );
 }
